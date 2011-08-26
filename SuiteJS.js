da0bc881e22d83aa4fb197830fb600f3fd3abad8
Greg <gsmith@incompl.com>
2011-08-26 06:12:32 -0400

    got the inline ctor right

--- SuiteJS.js
+++ SuiteJS.js
@@ -20,7 +20,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
-var suiteJS = function() {
+var suiteJS = new function() {
 
     this._lastPassed = true;
     this._passCount = 0;
